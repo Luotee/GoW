2d282dded204ec1b302a973c76bd51be88e747c7
Luotee <luote@DESKTOP-FR232SL.localdomain>
2019-12-03 18:44:34 +0800

    resize

--- lenna.cpp
+++ lenna.cpp
@@ -5,7 +5,12 @@ using namespace cv;
 
 int main()
 {
-    Mat img=imread(".\\lenna.png");
+    //讀取圖片
+    Mat img=imread(".\\hp.bmp");
+    //因windows scaling
+    resize(img, img, Size(800, 450), 0, 0, INTER_LINEAR);
+    namedWindow("image");
+	moveWindow("image", 0, 0);
     imshow("image",img);
     waitKey();
     return 0;
